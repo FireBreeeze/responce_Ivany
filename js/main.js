1aa54435f4bdeb519baf4d6f4b75782eddcd9270
FireBreeeze <amaslo111@gmail.com>
2023-06-13 00:05:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(function(){
+   
+//  $(".menu__link, .header__link, .logo").on("click", function(event){
+//         event.preventDefault();
+//         var id = $(this).attr('href'),
+//         top = $(id).offset().top;
+//         $('.header__top').toggleClass('header__top--fixed');
+//         $('body,html').animate({scrollTop: top-82}, 3500);
+//     });
+
+  $('.menu__button, .menu__link').on('click', function(){
+    $('.header__top').toggleClass('header__top--active');
+    $('body').toggleClass('lock');
+  });
+
+  $('.menu__link, .header__link, .logo').on("click", function(event){
+    event.preventDefault();
+    const scrollAnchor = $(this).attr('href');
+    let scrollPoint = $(scrollAnchor).offset().top;
+
+    if(scrollAnchor === '#gallery') {
+      scrollPoint = scrollPoint - 65;
+    } 
+
+    if(scrollAnchor === '#contacts') {
+      scrollPoint = scrollPoint - 65;
+    }
+
+    $('html, body').animate({scrollTop: scrollPoint}, 3500);
+    return false;
+  });
+
+  $window = $(window);
+  $window.scroll(function() {
+    $scrollPosition = $window.scrollTop();
+    if ($scrollPosition > 50) {
+      $('.header__top').addClass('header__top--bg');
+    } else {
+      $('.header__top').removeClass('header__top--bg');
+    }
+  });
+
+});
+
+// var mixer = mixitup('.gallery__content');
